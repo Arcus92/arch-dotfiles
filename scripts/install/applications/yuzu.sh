2955f8e0e4e3b4cf581887268e414fd30703129b
David Schulte <mail@david-schulte.de>
2023-02-25 23:44:00 +0100

    Fixed yuzu

--- scripts/install/applications/yuzu.sh
+++ scripts/install/applications/yuzu.sh
@@ -3,5 +3,8 @@
 # Requires yay
 source "${BASH_SOURCE%/*}/../tools/yay.sh"
 
+# Installing catch2 >= 3.xj
+yay -S --noconfirm catch2v3
+
 # Install yuzu (Switch emulator)
 yay -S --noconfirm yuzu-early-access
