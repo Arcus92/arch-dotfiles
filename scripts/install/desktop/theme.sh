4e658092fbd41e155757e038453bbc7e1e9d18e2
David Schulte <mail@david-schulte.de>
2023-02-26 15:19:45 +0100

    Fix QT themes

--- scripts/install/desktop/theme.sh
+++ scripts/install/desktop/theme.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm qt5ct qt6ct
+sudo pacman -S --needed --noconfirm qt5ct
 sudo pacman -S --needed --noconfirm adwaita-qt5 adwaita-qt6
-sudo pacman -S --needed --noconfirm breeze-gtk
+sudo pacman -S --needed --noconfirm breeze breeze-gtk
 
 # Install icons
-if [ ! -d "/usr/share/icons/Flatery" ]
+if [ ! -d "/usr/share/icons/Flatery-Dark" ]
 then
     git clone https://github.com/cbrnix/Flatery.git /tmp/Flatery
     sudo cp -rf /tmp/Flatery/Flatery-* /usr/share/icons/
