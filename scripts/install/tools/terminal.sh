00c6ecea009aeaca5c4be212d62a80da327bca24
David Schulte <mail@david-schulte.de>
2023-03-01 21:19:04 +0100

    Added fancy terminal tools

---
+++ scripts/install/tools/terminal.sh
@@ -0,0 +1,8 @@
+# Requires yay
+source "${BASH_SOURCE%/*}/../tools/yay.sh"
+
+# Install tty-clock ... a terminal clock for fancy screenshots
+yay -S tty-clock-git
+
+# Install cbonsai ... a terminal consai tree for fancy screenshots
+yay -S cbonsai-git
