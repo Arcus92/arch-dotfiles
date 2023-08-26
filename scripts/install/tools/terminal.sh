651730d725203a1a8cddc108f2f0f797388740c8
David Schulte <mail@david-schulte.de>
2023-08-26 13:35:03 +0200

    Fixed `noconfirm` for terminal.

--- scripts/install/tools/terminal.sh
+++ scripts/install/tools/terminal.sh
@@ -2,7 +2,7 @@
 source "${BASH_SOURCE%/*}/../tools/yay.sh"
 
 # Install tty-clock ... a terminal clock for fancy screenshots
-yay -S tty-clock-git
+yay -S --noconfirm tty-clock-git
 
 # Install cbonsai ... a terminal consai tree for fancy screenshots
-yay -S cbonsai-git
+yay -S --noconfirm cbonsai-git
