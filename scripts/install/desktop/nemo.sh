aa926426180b50d777760ba3803bbbf0cd9d6afb
David Schulte <mail@david-schulte.de>
2023-08-27 15:40:02 +0200

    Added nemo plugins.

--- scripts/install/desktop/nemo.sh
+++ scripts/install/desktop/nemo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # Install nemo (file manager)
-sudo pacman -S --needed --noconfirm nemo nemo-fileroller cinnamon-translations
+sudo pacman -S --needed --noconfirm nemo cinnamon-translations
+
+# Install nemo plugins
+sudo pacman -S --needed --noconfirm nemo-fileroller nemo-share
+
+# Install dependencies for network (e.g. SMB)
+sudo pacman -S --needed --noconfirm inetutils gvfs gvfs-smb gvfs-afc gvfs-gphoto2
