575c9c8f00b73f81a9d7769a51526acbdd6f673a
David Schulte <mail@david-schulte.de>
2023-10-03 00:18:07 +0200

    Added printer driver.

--- scripts/install/tools/printer.sh
+++ scripts/install/tools/printer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Requires yay
+source "${BASH_SOURCE%/*}/../tools/yay.sh"
+
 # Install cups and ghostscripts (printer service)
 sudo pacman -S --needed --noconfirm cups ghostscript
+
+# Install printer driver
+yay -S --noconfirm brother-dcpj772dw
 
 # Start cpus on startup
 sudo systemctl enable cups.service
