82d8f780db2ed0a276d397aeeb71f1ad01a38672
David Schulte <mail@david-schulte.de>
2024-04-06 15:23:33 +0200

    Install scanner driver.

--- scripts/install/tools/printer.sh
+++ scripts/install/tools/printer.sh
@@ -9,6 +9,12 @@ sudo pacman -S --needed --noconfirm cups ghostscript
 # Install printer driver
 yay -S --noconfirm brother-dcpj772dw
 
+# Install simple scan as scan application
+sudo pacman -S --needed --noconfirm simple-scan
+
+# Install brother scan driver
+yay -S --noconfirm brscan4
+
 # Start cpus on startup
 sudo systemctl enable cups.service
 sudo systemctl start cups.service
