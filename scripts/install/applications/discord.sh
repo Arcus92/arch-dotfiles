5d234f0c9e3585518a8b8d575f53e3ca08de7ea6
David Schulte <mail@david-schulte.de>
2024-04-20 15:24:01 +0200

    Added RPC fix for Discord

--- scripts/install/applications/discord.sh
+++ scripts/install/applications/discord.sh
@@ -11,3 +11,9 @@ sudo pacman -S --needed --noconfirm xdg-utils
 
 # Install Discord
 flatpak install -y com.discordapp.Discord
+
+# Enable RPC by adding a service to create a symlink to the internal flatpak rpc
+# Taken from: https://github.com/flathub/com.discordapp.Discord/wiki/Rich-Precense-(discord-rpc)
+mkdir -p ~/.config/user-tmpfiles.d
+echo 'L %t/discord-ipc-0 - - - - app/com.discordapp.Discord/discord-ipc-0' > ~/.config/user-tmpfiles.d/discord-rpc.conf
+systemctl --user enable --now systemd-tmpfiles-setup.service
