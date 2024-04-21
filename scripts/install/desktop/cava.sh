bf731d8ff0886622641251c8c26eca1a358b855a
David Schulte <mail@david-schulte.de>
2024-04-21 09:53:09 +0200

    Added beta hyprland config.

---
+++ scripts/install/desktop/cava.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Requires yay
+source "${BASH_SOURCE%/*}/../tools/yay.sh"
+
+# Install cava (Equaliser for waybar)
+yay -S --noconfirm cava
---
+++ scripts/install/desktop/hyprland.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Hyprland
+sudo pacman -S --needed --noconfirm hyprland
+
+# Install the Hyprland wallpaper tool
+sudo pacman -S --needed --noconfirm hyprpaper
