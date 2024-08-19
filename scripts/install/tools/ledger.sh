f2b97a7d164221284c0898b27f0370ee73085aee
David Schulte <mail@david-schulte.de>
2024-08-19 15:48:46 +0200

    Added yubico udev rules.

--- scripts/install/tools/ledger.sh
+++ scripts/install/tools/ledger.sh
@@ -7,4 +7,4 @@ sudo pacman -S --needed --noconfirm udev
 sudo cp -f "${BASH_SOURCE%/*}/../../../data/udev/20-ledger.rules" "/etc/udev/rules.d/20-ledger.rules"
 
 # Reload udev
-sudo udevadm control --reload-rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
--- scripts/install/tools/yubico.sh
+++ scripts/install/tools/yubico.sh
@@ -3,8 +3,8 @@
 # Install udev
 sudo pacman -S --needed --noconfirm udev
 
-# Adding udev rule for RCM
-sudo cp -f "${BASH_SOURCE%/*}/../../../data/udev/100-switch.rules" "/etc/udev/rules.d/100-switch.rules"
+# Adding udev rule for yubico
+sudo cp -f "${BASH_SOURCE%/*}/../../../data/udev/70-yubico.rules" "/etc/udev/rules.d/70-yubico.rules"
 
 # Reload udev
 sudo udevadm control --reload-rules && sudo udevadm trigger
